5793c6c7aa39f24dcd0f8e26498fd77c1d8bfa89
Yonahwang <634272867@qq.com>
2019-08-06 17:46:32 +0800

    add pca function

---
+++ CodeTest/dataSiens.js
@@ -0,0 +1,178 @@
+medicalList = [
+        {
+            "pointer": [
+                112.907175,
+                23.160284
+            ],
+            "name": "区人口计生服务站",
+            "address": "西南街道沙头涌南路5号",
+            "telphone": "0757-87765019",
+            "introduction": "x"
+        }
+
+medicalList = [
+        {
+            "pointer": [
+                112.888133,
+                23.176741
+            ],
+            "name": "区血站",
+            "address": "西南街道广海大道西16号",
+            "telphone": "0757-81812211",
+            "introduction": "x"
+        }
+
+            
+medicalList = [
+        {
+            "pointer": [
+                112.899343,
+                23.192036
+            ],
+            "name": "区卫生监督所",
+            "address": "云东海街道三达北路3号",
+            "telphone": "3670757-877718600",
+            "introduction": "x"
+        }
+
+medicalList = [
+    {
+        "pointer": [
+            112.898394,
+            23.168235
+        ],
+        "name": "区疾病防治",
+        "address": "西南街道赤岗路8号",
+        "telphone": "0757-87755198",
+        "introduction": "x"
+    }
+
+    medicalList = [
+        {
+            "pointer": [
+                112.827609,
+                23.096626
+            ],
+            "name": "金本民信医院",
+            "address": "佛山市三水区西南街金本五顶岗新风中路26号",
+            "telphone": "0757-87732384",
+            "introduction": "x"
+        }
+
+        
+        medicalList = [
+            {
+                "pointer": [
+                    112.891625,
+                    23.16037
+                ],
+                "name": "广州中医药大学附属佛山中医院三水医院",
+                "address": "佛山市三水区人民一路46号",
+                "telphone": "0757-87732384",
+                "introduction": "x"
+            }
+
+
+            medicalList = [
+                {
+                    "pointer": [
+                        112.843911,
+                        23.171718
+                    ],
+                    "name": "同方河口医院",
+                    "address": "佛山市三水区西门路与红城路交汇处北",
+                    "telphone": "0757-87732384",
+                    "introduction": "x"
+                }
+            
+
+medicalList = [
+    {
+        "pointer": [
+            112.84204,
+            23.071471
+        ],
+        "name": "佛山市三水区白坭镇人民医院",
+        "address": "佛山市三水区白坭镇府前路12号",
+        "telphone": "0757-87755198",
+        "introduction": "x"
+    }
+                
+
+    medicalList = [
+        {
+            "pointer": [
+                112.8873,
+                23.186194
+            ],
+            "name": "三水区恒德医院",
+            "address": "佛山市三水区广海大道西与三达路交汇处",
+            "telphone": "0757-87755198",
+            "introduction": "x"
+        }
+    
+        medicalList = [
+            {
+                "pointer": [
+                    112.893247,
+                    23.175655
+                ],
+                "name": "三水区女子(妇产)医院",
+                "address": "佛山市三水区碧湖花园",
+                "telphone": "0757-87755198",
+                "introduction": "x"
+            }
+        
+
+            medicalList = [
+                {
+                    "pointer": [
+                        113.025598,
+                        23.268679
+                    ],
+                    "name": "三水区乐平医院",
+                    "address": "佛山市三水区冠军广场",
+                    "telphone": "0757-87755198",
+                    "introduction": "x"
+                }
+
+                medicalList = [
+                    {
+                        "pointer": [
+                            112.902389,
+                            23.17424
+                        ],
+                        "name": "三水区妇幼保健院",
+                        "address": "佛山市三水区西南康乐路10号",
+                        "telphone": "0757-87755198",
+                        "introduction": "x"
+                    }
+                
+medicalList = [
+    {
+        "pointer": [
+            112.896678,
+            23.166199
+        ],
+        "name": "佛山市中医院(三水医院)",
+        "address": "佛山市三水区西南街道环城路2号",
+        "telphone": "0757-87755198",
+        "introduction": "x"
+    }
+                    
+    medicalList = [
+        {
+            "pointer": [
+                112.888133,
+                23.176741
+            ],
+            "name": "三水区人民医院(三水医院)",
+            "address": "佛山市三水区西南街道广海大道西16号",
+            "telphone": "0757-87755198",
+            "introduction": "x"
+        }
+     
+
+
+
+
